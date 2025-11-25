06ff99b27d9955e3bad99ea99d35fb71fec35180
sjourdbeans <sjoerdbuitjes@gmail.com>
2025-11-25 17:07:34 +0100

    Adjusted plot font

--- scripts/Flow/VisualiseFlowfield.py
+++ scripts/Flow/VisualiseFlowfield.py
@@ -16,7 +16,7 @@ mpl.rcParams['ytick.minor.visible'] = True
 import os
 os.environ["PATH"] += ":/usr/bin"
 mpl.rcParams['text.usetex'] = True
-mpl.rcParams["font.family"]= "DejaVu Sans"
+mpl.rcParams["font.family"]= "Palatino"
 mpl.rcParams["text.latex.preamble"]+= r"\usepackage{amsmath}"
 
 mpl.rcParams["xtick.labelsize"]=13
@@ -28,7 +28,7 @@ mpl.rcParams["legend.fontsize"]=13
 # Background flow
 U = np.zeros(3)
 
-U[0] = 1
+U[0] = -1
 U[1] = 0
 U[2] = 0
 
@@ -48,8 +48,8 @@ E = gamma_dot/2*np.array([[0,1,0],
                           [1,0,0],
                           [0,0,0]])
 
-path="/home/sjoerd-buitjes/University/Master-Thesis/BEM/Boundary-Element-Method/datafiles/mesh/elongated-mesh-fine/elongated_spheroid_N=320.mat"
-
+# path="/home/sjoerd-buitjes/University/Master-Thesis/BEM/Boundary-Element-Method/datafiles/mesh/elongated-mesh-fine/elongated_spheroid_N=320.mat"
+path="/home/sjoerd-buitjes/University/Master-Thesis/BEM/Boundary-Element-Method/datafiles/mesh/sphere_refinement/sphere_mesh_h=2.000000e-01.mat"
 plot_path = "/home/sjoerd-buitjes/University/Master-Thesis/Master-Thesis-Project/plots/Flowfield"
 plot_image_path = "/home/sjoerd-buitjes/University/Master-Thesis/Master-Thesis-Project/plot_images/Flowfield"
 # path="/home/sjoerd-buitjes/University/Master-Thesis/BEM/Boundary-Element-Method/datafiles/mesh/sphere_refinement/sphere_mesh_h=2.000000e-01.mat"
@@ -57,8 +57,8 @@ mesh= bem.Mesh(path)
 
 Nx=200 + 1
 Ny=200 + 1
-xlim=3
-ylim=3
+xlim=2
+ylim=2
 
 x = np.linspace(-xlim, xlim, Nx)
 y = np.linspace(-ylim, ylim, Ny)
@@ -83,15 +83,18 @@ psi, force, torque = sys.solve(U,W,E)
 
 U_field =interaction.calc_vector_field(psi,U, W, E)
 
-vmax=1.2*np.max(np.linalg.norm(U_field, axis =1))
+vmax=1#*np.max(np.linalg.norm(U_field, axis =1))
 # vmax=4
 
 fig = interaction.plot_vector_field(x, y, U_field, vmax= vmax, quiver_density=8)
 fig.set_size_inches(10,8)
 ax = fig.axes[0]
-ax.set_title(f"Flowfield with $\\mathbf{{u_{{\\infty}}}}= \\mathbf{{\\hat{{x}}}}$ $\\mu$m s$^{{-1}}$", fontsize=20)
-fig.savefig(f"{plot_path}/Flowfield_u_x={U[0]}.pdf")
-fig.savefig(f"{plot_image_path}/Flowfield_u_x={U[0]}.png",dpi=600)
+ax.set_title(f"Flowfield With $\\mathbf{{u}}^{{\\infty}}=-\\mathbf{{\\hat{{x}}}}$ $\\mu$s$^{{-1}}$", fontsize=20)
+ax.plot(mesh.isosurface[:,0],-mesh.isosurface[:,1],color='r')
+# ax.set_title(f"Shear Flow with $\\dot{{\\gamma}}={gamma_dot}$ s$^{{-1}}$", fontsize=20)
+plt.show()
+fig.savefig(f"{plot_path}/Flowfield_sphere_u_x={U[0]}.pdf")
+fig.savefig(f"{plot_image_path}/Flowfield_sphere_u_x={U[0]}.png",dpi=600)
 
 # plt.show()
 
