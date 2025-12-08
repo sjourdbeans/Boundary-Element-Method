43380e2d96162b12155af8a1c2d1ba160943d716
sjourdbeans <sjoerdbuitjes@gmail.com>
2025-12-08 11:18:52 +0100

    Added f1 and f2 as properties and adjusted the documentation

--- src/bemsolver/SaveData.py
+++ src/bemsolver/SaveData.py
@@ -5,7 +5,10 @@ from dataclasses import dataclass, field
 @dataclass
 class Solution:
     """
-    Dataclass to store solutions over time
+    Dataclass to store solutions over time. 
+    
+    NOTE : Some fields may contain empty arrays depending on the simulation. 
+    For example, if no flagellum is present, f1 and f2 will be empty arrays.
 
     Contains
     --------
@@ -19,6 +22,10 @@ class Solution:
                          Quaternion vectors over time (related to the rotation matrix)
     psi                : solution array (n, 3N) with N being the amount of elements
                          Singularity distribution over the surface of the mesh
+    f1                 : solution array (n, 3M) with M being the amount of elements of the flagellum
+                         Force distribution on each element of the flagellum.
+    f2                 : solution array (n, 3M) with M being the amount of elements of the flagellum
+                         Force distribution on each element of the flagellum.
     u                  : solution array (n,3)
                          Translational velocity of the particle over time
     omega              : solution array (n,3)
@@ -30,7 +37,7 @@ class Solution:
     rotation_matrices   :np.ndarray = field(default_factory=lambda: np.empty((0,)))
     quaternions         :np.ndarray = field(default_factory=lambda: np.empty((0,)))
     psi                 :np.ndarray = field(default_factory=lambda: np.empty((0,)))
+    f1                  :np.ndarray = field(default_factory=lambda: np.empty((0,)))
+    f2                  :np.ndarray = field(default_factory=lambda: np.empty((0,)))
     u                   :np.ndarray = field(default_factory=lambda: np.empty((0,)))
     omega               :np.ndarray = field(default_factory=lambda: np.empty((0,)))
-
-  
