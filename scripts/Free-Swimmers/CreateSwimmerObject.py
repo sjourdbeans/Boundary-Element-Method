2471a3987c219d27d917965f6d937b76c9163f9e
sjourdbeans <sjoerdbuitjes@gmail.com>
2026-07-16 14:58:09 +0200

    Updated the create swimmer object to use waveform of Wilson and Bees

--- scripts/Free-Swimmers/CreateSwimmerObject.py
+++ scripts/Free-Swimmers/CreateSwimmerObject.py
@@ -30,180 +30,88 @@ mpl.rcParams["legend.fontsize"]=13
 
 
 
-waveformfile      = loadmat("/home/sjoerd-buitjes/University/Master-Thesis/BEM/Boundary-Element-Method/datafiles/waveform/Chlamy-2D/lib02_1_90_2019-06-28_1640.mat")
 chlamy_path       = "/home/sjoerd-buitjes/University/Master-Thesis/BEM/Boundary-Element-Method/datafiles/mesh/Chlamy/chlamy_N=320.mat"
+# chlamy_path       = "/home/sjoerd-buitjes/University/Master-Thesis/BEM/Boundary-Element-Method/datafiles/mesh/Chlamy/sphere_chlamy_N=320.mat"
+
+movie_data="/home/sjoerd-buitjes/University/Master-Thesis/BEM/Boundary-Element-Method/datafiles/waveform/Chlamy-3D/CC125_4_2_T_avg_int.txt"
+movie_data_2="/home/sjoerd-buitjes/University/Master-Thesis/BEM/Boundary-Element-Method/datafiles/waveform/Chlamy-3D/CC125_4_2_C_avg_int.txt"
+coords =np.loadtxt(movie_data)
+coords_2 =np.loadtxt(movie_data_2)
 
 
-cell = waveformfile["Cell"]
-thetar = cell["thetar"].item()[0][0]
-thetal = cell["thetal"].item()[0][0]
-# print(thetal)
+theta=np.pi/2
+rotmat=np.array([[np.cos(theta), -np.sin(theta),0],
+                 [np.sin(theta), np.cos(theta),0],
+                 [0,0,1]])
+lf=12
 
-phi_body = cell["phi_body"].item()[0][0]
-# print(phi_body)
+R = coords.reshape(int(len(coords)/20),20,3)/7 * lf
 
-xbase =-5#  cell["dist_base"].item()[0][0]
-# print(xbase)
-ybase = 0
-zbase = 0
+# the second flagella
 
+# R2 = coords_2.reshape(int(len(coords_2)/20),20,3)/ 7 *lf
 
+# mirror the first flagellum to make a symmetric swimmer
+R2 = R.copy()
+R2[:,:,0] *= -1
+R2[:,:,2] *= -1
 
-lf = waveformfile["lf0"][0,0]
-# lf = 12
-fps = waveformfile["fps"][0]
-dt = 1 /fps
+# R = R[::2,:,:]
+# R2 =R2[::2,:,:]
 
+dt=400*10**(-6)
 
-def find_flow(t: float, x: np.ndarray)->tuple[np.ndarray, np.ndarray, np.ndarray]:
-    # no shear flow
-    gamma_dot=0
+#calculat flagellar velocities
 
-    U = np.zeros(3)
+V = (np.roll(R, -1, axis=0) - 
+     np.roll(R,  1, axis=0)) / (2*dt)
 
-    U[0] = 0
-    U[1] = 0
-    U[2] = 0
+V2 = (np.roll(R2, -1, axis=0) - 
+     np.roll(R2,  1, axis=0)) / (2*dt)
 
-    # Background vorticity
-    W = np.zeros(3)  
-
-    W[0] = 0
-    W[1] = 0
-    W[2] = -gamma_dot
-
-    # Rate of strain tensor
-    E = gamma_dot/2*np.array([[0,1,0],
-                            [1,0,0],
-                            [0,0,0]])
-    return U, W, E
 
 
 
 flagellum_1 = []
 flagellum_2 = []
 
-N_frames = len(waveformfile["kappasave"])
+N_frames = len(R)
 
-Rotmat = lambda angle: np.array([[1, 0, 0],
-                                 [ 0, np.cos(angle), -np.sin(angle)],
-                                 [0, np.sin(angle), np.cos(angle)]])
+mesh = bem.Mesh(chlamy_path)
+
 
 # loop over all frames to create flagellum objects
 for frame in range(N_frames):
-    # frame=15
-    # Set flagellum velocities
-    # velx_1 = waveformfile["velx0"][frame,0] * lf
-    # vely_1 = waveformfile["vely0"][frame,0] * lf
-    # velz_1 = np.zeros_like(vely_1) * lf
 
-    velx_1 =- waveformfile["velx0"][frame,0] * lf
-    vely_1 =- waveformfile["vely0"][frame,0] * lf
-    velz_1 = np.zeros_like(vely_1) * lf
+    R[frame]=(rotmat @ (R[frame]-R[frame][0]).T ).T +np.array([5,1,0])
 
-    vel_1 = np.vstack([velx_1, vely_1, velz_1]).T #* mu
+    R2[frame]=(rotmat @ (R2[frame]-R2[frame][0]).T  ).T+np.array([5,-1,0])
 
-    vels_1=np.zeros_like(vel_1)
+    v= (rotmat @ V[frame].T ).T
+    v2=(rotmat @ V2[frame].T).T
 
-    # velx_2 = waveformfile["velx0"][frame,1] * lf
-    # vely_2 = -waveformfile["vely0"][frame,1] * lf
-    # velz_2 = np.zeros_like(vely_1) * lf
-
-    velx_2 = -waveformfile["velx0"][frame,1] * lf
-    vely_2 = waveformfile["vely0"][frame,1] * lf
-    velz_2 = np.zeros_like(vely_1) * lf
-
-    vel_2 = np.vstack([velx_2, vely_2, velz_2]).T #* mu
-    vels_2 =np.zeros_like(vel_2)
-    # set flagellum shapes and positions
-    base_position_1 = np.array([xbase , ybase, zbase]) 
-    curv_1 = waveformfile["kappasave"][frame,0,1:] 
-    theta_0_1 = waveformfile["kappasave"][frame,0,0]
-
-    base_position_2 = base_position_1 
-    curv_2 = -waveformfile["kappasave"][frame,1,1:] 
-    theta_0_2 = waveformfile["kappasave"][frame,1,0]
-
-    initial_angle_1 =  np.pi- (thetal - phi_body) + theta_0_1
-    initial_angle_2 =  np.pi- (thetar - phi_body) - theta_0_2
-
-    #=======Symmetric flagella=========
-
-
-    # velx_1 = waveformfile["velx0"][frame,0] * lf
-    # vely_1 = waveformfile["vely0"][frame,0] * lf
-    # velz_1 = np.zeros_like(vely_1) * lf
-
-    # vel_1 = np.vstack([velx_1, vely_1, velz_1]).T #* mu
-
-    # vels_1=np.zeros_like(vel_1)
-
-    # velx_2 = velx_1
-    # vely_2 = -vely_1
-    # velz_2 = np.zeros_like(vely_1) * lf
-
-    # vel_2 = np.vstack([velx_2, vely_2, velz_2]).T #* mu
-    # vels_2 =np.zeros_like(vel_2)
-    # # set flagellum shapes and positions
-    # base_position_1 = np.array([xbase , ybase, zbase]) 
-    # curv_1 = waveformfile["kappasave"][frame,0,1:] 
-    # theta_0_1 = waveformfile["kappasave"][frame,0,0]
-
-    # base_position_2 = base_position_1 
-    # curv_2 = -curv_1
-    # # theta_0_2 = waveformfile["kappasave"][frame,1,0]
-
-    # initial_angle_1 =  - (thetal - phi_body) + theta_0_1
-    # initial_angle_2 = - initial_angle_1
-    # initial_angle_2 =  - (thetal - phi_body) - theta_0_1
-    #==================================
-
-
-    tors_1 = np.zeros_like(curv_1)
-    # tors_1[0]=np.pi
-    tors_2 = np.zeros_like(curv_1)
-    # tors_2[0]=-np.pi
-
-    # vel_1 = (Rotmat(np.pi/2) @ vel_1.T).T
-    # vel_2 = (Rotmat(np.pi/2) @ vel_2.T).T
-
-
-    flag1 = bem.SlenderCurvTors(curv_1,tors_1,
-                           theta_0=initial_angle_1,
-                           flagellum_length=lf,
-                           base_position=base_position_1,
-                           velocity=vel_1,
-                           flagellum_radius=0.1,smin=0)
+    flag1 = bem.SlenderCoordinates(R[frame],v, flagellum_length=lf,flagellum_radius=0.2, smin=0)
     
-    flag2 = bem.SlenderCurvTors(curv_2, tors_2,
-                            theta_0=initial_angle_2,
-                            flagellum_length=lf,
-                            base_position=base_position_1,
-                            velocity=vel_2,
-                            flagellum_radius=0.1,smin=0)
-    
-    # flag1.r = (Rotmat(np.pi/2) @ flag1.r.T ).T
-    # flag1.tangents = (Rotmat(np.pi/2) @flag1.tangents.T ).T
-    
+    flag2 = bem.SlenderCoordinates(R2[frame],v2, flagellum_length=lf,flagellum_radius=0.2, smin=0)
 
-    # flag2.r = (Rotmat(np.pi/2) @ flag2.r.T).T
-    # flag2.tangents = (Rotmat(np.pi/2) @ flag2.tangents.T).T
-
-    flagellum_1.append(flag1)   
+    flagellum_1.append(flag1)
     flagellum_2.append(flag2)
 
 # ===================Create swimmer object=====================
-mesh = bem.Mesh(chlamy_path)
+
 
 chlamy = bem.FreeSwimmer(mesh,
                         flagellum_1=flagellum_1,flagellum_2=flagellum_2)
+
+# chlamy = bem.Swimmer(mesh,
+#                     flagellum_1=flagellum_1,flagellum_2=flagellum_2)
 # =============================================================
 
 
 # ===================Save option 1=====================
 
 # Save swimmer object without results (large file)
-with open("/home/sjoerd-buitjes/University/Master-Thesis/Master-Thesis-Project/Data/BEM/python-BEM/swimmer-objects/Chlamy/free/chlamy_free_2D_waveform.pkl", "wb") as f:
+with open(f"/home/sjoerd-buitjes/University/Master-Thesis/Master-Thesis-Project/Data/BEM/python-BEM/swimmer-objects/Chlamy/free/chlamy_free_3d.pkl", "wb") as f:
     pickle.dump(chlamy, f)
 
 
